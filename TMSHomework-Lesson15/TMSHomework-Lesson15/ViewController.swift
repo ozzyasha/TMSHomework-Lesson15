c7977e2fa7acbb39ac96aa226398f57ac33e3d1e
Natallia Mazur <natashamazur@bk.ru>
2024-01-11 14:14:28 +0300

    New project has been created

---
+++ TMSHomework-Lesson15/TMSHomework-Lesson15/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TMSHomework-Lesson15
+//
+//  Created by Наталья Мазур on 11.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+// ДЗ:
+// Создать кастомную view в виде алерта.
+// Попробовать ее показывать и скрывать по нажатию на кнопку.
